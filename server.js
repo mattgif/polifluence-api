9cd71efdae9a77205ccc88c4e79cc51a8ccbf05c
Matt Gifford <mattgif@gmail.com>
2018-05-03 13:02:08 -0400

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get('/api/*', (req, res) => {
     res.json({ok: true});
