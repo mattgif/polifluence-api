e3aca6d15f14b4445e3ad79c9176c03f9bc58885
Matt Gifford <mattgif@gmail.com>
2018-05-03 14:22:14 -0400

    added dotenv

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports.OPEN_SECRETS_API_KEY=process.env.OPEN_SECRETS_API_KEY;
+module.exports.MAPLIGHT_API_KEY=process.env.MAPLIGHT_API_KEY;
+module.exports.PROPUBLICA_API_KEY=process.env.PROPUBLICA_API_KEY;
